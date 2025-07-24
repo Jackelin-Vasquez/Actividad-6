2e25cef434097cfc6a29ad9453a21ba4122c94e6
Jackelin <jackelinvasquezguzman@gmail.com>
2025-07-24 09:32:40 -0600

    Actividad 6

--- actividad_6.py
+++ actividad_6.py
@@ -1,1 +1,10 @@
 #Actividad no.6
+def menu():
+    print("1.Ingresar números y mostrar.")
+    print("2.Calcular el área de un triángulo.")
+    print("3.Vereficar si es un número par i impar.")
+    print("4.Calcular el promedio de calificaciones")
+    print("5.Ingresar n números y mostrar cual es mayor y menor")
+    print("6.Salir del programa")
+
+def
